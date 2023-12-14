627d4483343aa5cfb8cfa5888590198e3e838eb3
Eduardo <edureisfaria@gmail.com>
2023-12-14 03:49:21 +0000

    New Look
    Added Images for Nav Bar
    Updated Storyboard to Nav bar
    Updated HomeView Controller
    Updated Devides Properties of API

--- SmartGrow/Controllers/HomeViewController.swift
+++ SmartGrow/Controllers/HomeViewController.swift
@@ -9,8 +9,25 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    @IBOutlet weak var imgLeft: UINavigationItem!
+    
+    @IBOutlet weak var imgCenter: UINavigationItem!
+    
+    @IBOutlet weak var imgRight: UINavigationItem!
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Load images from assets
+        let NavBar_Home = UIImage(named: "NavBar_Home")
+        let NavBar_Logo = UIImage(named: "NavBar_Logo")
+        let NavBar_Search = UIImage(named: "NavBar_Search")
+        
+        imgLeft.leftBarButtonItem = UIBarButtonItem(image: NavBar_Home, style: .plain, target: nil, action: nil);
+        imgCenter.titleView = UIImageView(image: NavBar_Logo);
+        imgRight.rightBarButtonItem = UIBarButtonItem(image: NavBar_Search, style: .plain, target: nil, action: nil);
+        
         // Do any additional setup after loading the view.
     }
 
