bc03af300ab7491c36d59e78ce17a392e9c244c7
Eduardo <edureisfaria@gmail.com>
2024-01-03 17:13:39 +0000

    Fixed Detail Presentation
    Fixed Navigation

--- SmartGrow/Controllers/DetailViewController.swift
+++ SmartGrow/Controllers/DetailViewController.swift
@@ -14,6 +14,8 @@ class DetailViewController: UIViewController {
     
     @IBOutlet weak var imgRight: UINavigationItem!
     
+    var imei: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
